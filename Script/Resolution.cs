56b2fb88779db50df4a4482f2301227f4b1d2446
Piotr Kachelia <vanyapingenin@gmail.com>
2020-03-08 18:48:39 +0300

    First commit

---
+++ Script/Resolution.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolution : MonoBehaviour
+{
+    public int xResolution = 1280, yResolution = 720;
+
+    void Awake()
+    {
+        ResolutionOnAwake();
+    }
+
+    public void ResolutionOnAwake()
+    {
+        Screen.SetResolution(xResolution, yResolution, true);
+
+        Camera.main.aspect = 16f / 9f;
+    }
+}
